79fa216f0e4d2eb830601f2368257bee5297ae64
Zharkynbek Idirisov <zh.idirisov@gmail.com>
2021-07-25 17:32:39 +0200

    get start tu  use hooks in loginView

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,68 +1,66 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button } from "@material-ui/core";
-import { connect } from "react-redux";
-import authOperations from "../redux/auth/auth-operations";
+// import { connect } from "react-redux";
+// import authOperations from "../redux/auth/auth-operations";
 
-class LoginView extends Component {
-  state = {
-    email: "",
-    password: "",
+function LoginView() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // componentDidMount() {
+  //   this.setState({
+  //     email: "klklklkl@mail.com",
+  //     password: "klklklkl123",
+  //   });
+  // }
+
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
   };
 
-  componentDidMount() {
-    this.setState({
-      email: "klklklkl@mail.com",
-      password: "klklklkl123",
-    });
-  }
-
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
+  const updatePassword = (e) => {
+    setPassword(e.target.value);
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.props.onLogin(this.state);
-    this.setState({ name: "", email: "", password: "" });
+    alert(`${email}, ${password}`);
   };
 
-  render() {
-    const { email, password } = this.state;
+  return (
+    <div>
+      <h1>Please, Sign In</h1>
+      <form onSubmit={handleSubmit} autoComplete="off">
+        <label>
+          <p>Email</p>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={updateEmail}
+          />
+        </label>
 
-    return (
-      <div>
-        <h1>Please, Sign In</h1>
-        <form onSubmit={this.handleSubmit} autoComplete="off">
-          <label>
-            <p>Email</p>
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={this.handleChange}
-            />
-          </label>
-
-          <label>
-            <p>Password</p>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={this.handleChange}
-            />
-          </label>
-          <br />
-          <Button type="submit" variant="contained" color="secondary">
-            Sign In
-          </Button>
-        </form>
-      </div>
-    );
-  }
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={updatePassword}
+          />
+        </label>
+        <br />
+        <Button type="submit" variant="contained" color="secondary">
+          Sign In
+        </Button>
+      </form>
+    </div>
+  );
 }
 
-const mapDispatchToProps = {
-  onLogin: authOperations.login,
-};
-export default connect(null, mapDispatchToProps)(LoginView);
+// const mapDispatchToProps = {
+//   onLogin: authOperations.login,
+// };
+// export default connect(null, mapDispatchToProps)(LoginView);
+export default LoginView;
